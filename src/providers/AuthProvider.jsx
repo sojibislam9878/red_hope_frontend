441755082cdacbeffe9ca89b22304b6661b6c20d
sojibislam9878 <sojibhasan9878@gmail.com>
2025-09-16 21:13:02 +0600

    signup and login part completed with social and manual

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,41 @@
 import { createContext, useEffect, useState } from "react";
 import { FacebookAuthProvider, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../firebase/FirebaseConfiq";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
+  const [userData, setUserData]=useState(null)
   const [logedInUser, setLogedInUser] = useState(null)
   const [authLoading, setAuthLoading] = useState(false);
+  
+  useEffect(() => {
+    const signupUser = async () => {
+      if (logedInUser) {
+        const userData = {
+          firstName: logedInUser?.displayName,
+          lastName: "",
+          email: logedInUser?.email,
+          photo: logedInUser?.photoURL,
+          phone: "",
+          provider: logedInUser?.providerData[0]?.providerId,
+          terms: true,
+        };
+        
+        try {
+          const res = await axios.post(
+            "http://localhost:3000/social-login",
+            userData
+          );
+          setUserData(res.data.user);
+        } catch (err) {
+          console.log(err.response?.data || "Signup failed");
+        }
+      }
+    };
+
+    signupUser();
+  }, [logedInUser]); 
   
   const googleProvider = new GoogleAuthProvider();
   const googleSignUP = () => {
@@ -28,14 +58,16 @@ const AuthProvider = ({ children }) => {
   }, []);
 
   const logout = () => {
-    setLogedInUser(null);
+    setUserData(null);
     signOut(auth);
   };
 
   const values = {
     googleSignUP,
     facebookSingUP,
-    logedInUser,
+    userData,
+    setLogedInUser,
+    setUserData,
     logout,
   };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
