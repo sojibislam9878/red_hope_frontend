f20247f989248ddf4444e4cbd83c9ed609a9e9e6
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2025-09-20 22:54:59 +0600

    change logo for dark and add a custom hook for theme and language control

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from './routes/Routes.jsx';
-import AuthProvider from './providers/AuthProvider.jsx';
+import { router } from "./routes/Routes.jsx";
+import AuthProvider from "./providers/AuthProvider.jsx";
+import { PageConfigProvider } from "./hooks/PageConfigProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router}/>
+      <PageConfigProvider>
+        <RouterProvider router={router} />
+      </PageConfigProvider>
     </AuthProvider>
   </StrictMode>
 );
